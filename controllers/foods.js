14b90f56d0447660a58907d4a788c066ce11eda2
PeterT <tranpp87@gmail.com>
2021-11-05 18:39:08 -0400

    reroute to food index upon signin. create home route

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,13 +1,27 @@
 import { Food } from '../models/food.js'
 
+function index(req, res){
+  console.log('home page request')
+  res.render('foods/index',{
+    title: "All Foods"
+  })
+}
+
 function newFood(req, res) {
   console.log("Add New Food")
   res.render('foods/new', {
     title: "Enter Food",
-    
   })
 }
 
+function create(req, res){
+  console.log('creating a new food review')
+  res.redirect('/foods')
+}
+
+
 export {
+  index,
   newFood as new,
+  create,
 }
--- routes/foods.js
+++ routes/foods.js
@@ -3,7 +3,14 @@ import * as foodsCtrl from '../controllers/foods.js'
 
 const router = Router()
 
+//localhost:3000/foods
+router.get('/', foodsCtrl.index)
+
+// localhost:3000/foods/mew
 router.get('/new', foodsCtrl.new)
+
+// localhost:3000/foods
+router.post('/', foodsCtrl.create)
 
 export{
   router
