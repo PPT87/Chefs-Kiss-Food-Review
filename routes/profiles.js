5d984bd6035e494e1ed26f0e056ca138213c1a92
PeterT <tranpp87@gmail.com>
2021-11-06 14:28:45 -0400

    setup profile view, router and controller

---
+++ routes/profiles.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as profilesCtrl from "../controllers/profiles.js"
+
+const router = Router()
+
+// localhost:3000/community - GET
+router.get('/', isLoggedIn, profilesCtrl.index)
+
+
+
+export {
+  router
+}
